665ee52efab392422d52728730596b2365c0755a
Jeronimo S Avelar Filho <jeroavf@gmail.com>
2018-09-11 06:24:06 -0300

    Show all books

--- bookmanager.py
+++ bookmanager.py
@@ -29,7 +29,8 @@ def home():
         print( title )
         #print(request.form)
 
-    return render_template("home.html")
+    books = Book.query.all()     
+    return render_template("home.html" , books=books)
 
 if __name__ == "__main__":
     app.run(debug=True)
